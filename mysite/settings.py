1afcefbbb892016cfdfeabb84436ea9604446935
Jake2135 <34521031+Jake2135@users.noreply.github.com>
2018-08-17 12:42:56 -0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': timecarddeck,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
